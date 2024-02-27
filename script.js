b007222ea205cb2b4520c733a2e863643123e6be
Paul <161381201+bonneruepaul@users.noreply.github.com>
2024-02-27 12:18:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.getElementById('loader').style.display = "flex";
+  } else {
+    document.getElementById('loader').style.display = "none";
+  }
+};
